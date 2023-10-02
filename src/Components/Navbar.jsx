8570d6374f91205dd4db8ca5933aa0ca1d93578a
Albertosl2497 <146301297+Albertosl2497@users.noreply.github.com>
2023-10-02 11:24:15 -0700

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,8 +7,8 @@ function Navbar() {
       <div className="row">
         <img src={Logo} alt="Logo" />
         <div className="col">
-          <h1>SORTEOS </h1>
-          <h1>SONORENSE</h1>
+          <h1>RIFAS EFECTIVO </h1>
+          <h1>CAMPO TREINTA</h1>
         </div>
       </div>
     </nav>
