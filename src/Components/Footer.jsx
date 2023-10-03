8e32a7e19fddf37c4940f2915ac9434f176d0a07
Albertosl2497 <146301297+Albertosl2497@users.noreply.github.com>
2023-10-03 12:53:05 -0700

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -8,14 +8,16 @@ function Footer() {
           
           <div className="col-md-4 col-md-6 center">
             <h2 className="white bold">METODOS DE PAGO:</h2>
+            <div className="col" style={{ display: 'flex', flexDirection: 'column' }}>
             <h2 className="white bold">1.- TRANSFERENCIAS:</h2>
-            <p className="bold" style={{ fontSize: '22px' }}>CUENTA: </p>
-            <p className="bold" style={{ fontSize: '22px' }}>BANCO: STP</p>
-            <p className="bold" style={{ fontSize: '22px' }}>NOMBRE: MARIA RUIZ BORQUEZ</p>
+            <p className="bold" style={{ fontSize: '20px' }}>CUENTA: </p>
+            <p className="bold" style={{ fontSize: '20px' }}>BANCO: STP</p>
+            <p className="bold" style={{ fontSize: '20px' }}>NOMBRE: MARIA RUIZ BORQUEZ</p>
 
-            <p className="bold" style={{ fontSize: '22px' }}>CUENTA: </p>
-            <p className="bold" style={{ fontSize: '22px' }}>BANCO: STP</p>
-            <p className="bold" style={{ fontSize: '22px' }}>NOMBRE: MARTIN SANCHEZ LEYVA</p>
+            <p className="bold" style={{ fontSize: '20px' }}>CUENTA: </p>
+            <p className="bold" style={{ fontSize: '20px' }}>BANCO: STP</p>
+            <p className="bold" style={{ fontSize: '20px' }}>NOMBRE: MARTIN SANCHEZ LEYVA</p>
+          <div/>
 
 
 
