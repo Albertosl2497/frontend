8d3be7029355121b611e00cb7d6a4bd1d4ed0712
Albertosl2497 <146301297+Albertosl2497@users.noreply.github.com>
2023-10-03 13:10:07 -0700

    Update HomePage.jsx

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,40 +1,45 @@
-import React, { useState } from "react";
+import React from "react";
 import Navbar from "../Components/Navbar";
 import TicketForm from "../Components/TicketForm";
 import Footer from "../Components/Footer";
 
-function HomePage({ tickets, loading, lotteryNo, setTickets }) {
+function MainPageContent() {
   return (
-    <div>
-      {/* Navbar */}
-      <Navbar />
+    <div className="col" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+      <p className="bold" style={{ fontSize: '24px' }}>GRAN EDICIÓN # 12</p>
+      <p className="bold" style={{ fontSize: '24px' }}>ESTE 31 DE OCTUBRE</p>
+      <p className="bold" style={{ fontSize: '24px' }}>LLEVATE $20,000 PESOS</p>
+      <p style={{ fontSize: '20px' }}>Costo por Boleto $35.00 MXN</p>
+      <br />
+    </div>
+  );
+}
 
-      <div className="post-nav-stuff">
-        {/* Main */}
-        <div className="col" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-  <p className="bold" style={{ fontSize: '24px' }}>GRAN EDICIÓN # 12</p>
-  <p className="bold" style={{ fontSize: '24px' }}>ESTE 31 DE OCTUBRE</p>
-  <p className="bold" style={{ fontSize: '24px' }}>LLEVATE $20,000 PESOS</p>
-  <p style={{ fontSize: '20px' }}>Costo por Boleto $35.00 MXN</p>
-  <br />
-  
-</div>
-<div className="col" style={{ display: 'flex', flexDirection: 'column'}}>
-        <p className="bold" style={{ fontSize: '22px' }}>Realiza los siguientes pasos:</p>
-  <p className="bold" style={{ fontSize: '18px' }}>PASO 1: Llena el formulario con tus datos:</p>
-  <p style={{ fontSize: '16px' }}>
+function StepsToFollow() {
+  return (
+    <div className="col" style={{ display: 'flex', flexDirection: 'column' }}>
+      <p className="bold" style={{ fontSize: '22px' }}>Realiza los siguientes pasos:</p>
+      <p className="bold" style={{ fontSize: '18px' }}>PASO 1: Llena el formulario con tus datos:</p>
+      <p style={{ fontSize: '16px' }}>
         - Teléfono. <br />
         - Nombre y apellidos. <br />
         - Estado. <br />
         - Ciudad. <br />
         - Correo Electrónico (Si no cuentas con uno, deja el que te aparece).
-  </p>
-  <p className="bold" style={{ fontSize: '18px' }}>PASO 2: Selecciona tus Boletos.</p>
-  <p className="bold" style={{ fontSize: '18px' }}>PASO 3: Presiona el Botón Verde (Apartar Boletos).</p>
-</div>
+      </p>
+      <p className="bold" style={{ fontSize: '18px' }}>PASO 2: Selecciona tus Boletos.</p>
+      <p className="bold" style={{ fontSize: '18px' }}>PASO 3: Presiona el Botón Verde (Apartar Boletos).</p>
+    </div>
+  );
+}
 
-
-        {/* Form */}
+function HomePage({ tickets, loading, lotteryNo, setTickets }) {
+  return (
+    <div>
+      <Navbar />
+      <div className="post-nav-stuff">
+        <MainPageContent />
+        <StepsToFollow />
         <TicketForm
           tickets={tickets}
           loading={loading}
@@ -42,7 +47,6 @@ function HomePage({ tickets, loading, lotteryNo, setTickets }) {
           setTickets={setTickets}
         />
       </div>
-
       <Footer />
     </div>
   );
