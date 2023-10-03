22064975184de6c177757253b243c7d29e668f0e
Albertosl2497 <146301297+Albertosl2497@users.noreply.github.com>
2023-10-03 12:57:03 -0700

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,28 +5,23 @@ function Footer() {
     <footer>
       <div className="footer-container">
         <div className="row">
-          
           <div className="col-md-4 col-md-6 center">
             <h2 className="white bold">METODOS DE PAGO:</h2>
             <div className="col" style={{ display: 'flex', flexDirection: 'column' }}>
-            <h2 className="white bold">1.- TRANSFERENCIAS:</h2>
-            <p className="bold" style={{ fontSize: '20px' }}>CUENTA: </p>
-            <p className="bold" style={{ fontSize: '20px' }}>BANCO: STP</p>
-            <p className="bold" style={{ fontSize: '20px' }}>NOMBRE: MARIA RUIZ BORQUEZ</p>
-
-            <p className="bold" style={{ fontSize: '20px' }}>CUENTA: </p>
-            <p className="bold" style={{ fontSize: '20px' }}>BANCO: STP</p>
-            <p className="bold" style={{ fontSize: '20px' }}>NOMBRE: MARTIN SANCHEZ LEYVA</p>
-          <div/>
-
-
-
-            
-            
-           
-            
+              <div>
+                <h2 className="white bold">1.- TRANSFERENCIAS:</h2>
+                <p className="bold" style={{ fontSize: '20px' }}>CUENTA: 64-6760-1464-0398-4695</p>
+                <p className="bold" style={{ fontSize: '20px' }}>BANCO: STP</p>
+                <p className="bold" style={{ fontSize: '20px' }}>NOMBRE: MARIA RUIZ BORQUEZ</p>
+              </div>
+              <div>
+                <h2 className="white bold">2.- TRANSFERENCIAS:</h2>
+                <p className="bold" style={{ fontSize: '20px' }}>CUENTA: 64-6760-1464-0337-9116</p>
+                <p className="bold" style={{ fontSize: '20px' }}>BANCO: STP</p>
+                <p className="bold" style={{ fontSize: '20px' }}>NOMBRE: MARTIN SANCHEZ LEYVA</p>
+              </div>
+            </div>
           </div>
-          
         </div>
       </div>
     </footer>
