270bcca96c82e112691365ad810754ab47a9ff30
Albertosl2497 <146301297+Albertosl2497@users.noreply.github.com>
2023-10-03 13:02:22 -0700

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -8,17 +8,27 @@ function Footer() {
           <div className="col-md-4 col-md-6 center">
             <h2 className="white bold">METODOS DE PAGO:</h2>
             <div className="col" style={{ display: 'flex', flexDirection: 'column' }}>
-              <div>
-                <h2 className="white bold">1.- TRANSFERENCIAS:</h2>
+              <div className="col">
+                <h2 className="white bold">TRANSFERENCIAS 1:</h2>
                 <p className="bold" style={{ fontSize: '20px' }}>CUENTA: 64-6760-1464-0398-4695</p>
                 <p className="bold" style={{ fontSize: '20px' }}>BANCO: STP</p>
                 <p className="bold" style={{ fontSize: '20px' }}>NOMBRE: MARIA RUIZ BORQUEZ</p>
               </div>
-              <div>
-                <h2 className="white bold">2.- TRANSFERENCIAS:</h2>
+              <div className="col">
+                <h2 className="white bold">TRANSFERENCIAS 2:</h2>
                 <p className="bold" style={{ fontSize: '20px' }}>CUENTA: 64-6760-1464-0337-9116</p>
                 <p className="bold" style={{ fontSize: '20px' }}>BANCO: STP</p>
                 <p className="bold" style={{ fontSize: '20px' }}>NOMBRE: MARTIN SANCHEZ LEYVA</p>
+              </div>
+              <div className="col">
+                <h2 className="white bold">DEPOSITOS EN OXXO 2:</h2>
+                <p className="bold" style={{ fontSize: '20px' }}>CUENTA: 2242-1707-6018-2351</p>
+                
+              </div>
+              <div className="col">
+                <h2 className="white bold">DEPOSITOS EN OXXO 2:</h2>
+                <p className="bold" style={{ fontSize: '20px' }}>CUENTA: 2242-1707-6024-2905</p>
+                
               </div>
             </div>
           </div>
