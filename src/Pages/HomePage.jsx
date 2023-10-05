bacb32a827ebe3710270c97b55bb3a0d59b64489
Albertosl2497 <146301297+Albertosl2497@users.noreply.github.com>
2023-10-05 13:08:36 -0700

    Update HomePage.jsx

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -6,7 +6,7 @@ import Footer from "../Components/Footer";
 function MainPageContent() {
   return (
     <div className="col" style={{ display: 'flex', flexDirection: 'column' }}>
-      <p className="bold" style={{ fontSize: '24px' }}>GRAN EDICIÓN # 12</p>
+      <p className="bold" style={{ fontSize: '24px' }}>GRAN EDICIÓN # 13</p>
       <p className="bold" style={{ fontSize: '24px' }}>ESTE 31 DE OCTUBRE</p>
       <p className="bold" style={{ fontSize: '24px' }}>LLEVATE $20,000 PESOS</p>
       <p style={{ fontSize: '20px' }}>Costo por Boleto $35.00 MXN</p>
@@ -22,7 +22,7 @@ function StepsToFollow() {
       <p className="bold" style={{ fontSize: '22px' }}>Realiza los siguientes pasos:</p>
       <p className="bold" style={{ fontSize: '18px' }}>
         PASO 1: Llena el formulario con tus datos:</p>
-      <p style={{ fontSize: '16px' }}>
+      <p style={{  fontWeight: 'bold', fontSize: '16px' }}>
         - Teléfono. <br />
         - Nombre y apellidos. <br />
         - Estado. <br />
@@ -33,7 +33,7 @@ function StepsToFollow() {
         PASO 2: Selecciona tus Boletos.               </p>
       <p className="bold" style={{ fontSize: '18px' }}>
         PASO 3: Presiona el Botón Verde (Apartar Boletos).     </p>
-      <p  style={{ fontSize: '16px' }}>
+      <p  style={{ fontWeight: 'bold', fontSize: '16px' }}>
         NOTA: Al realizar esta serie de pasos y presionar en (Apartar Boletos) seras redireccionado a Whatsapp,
         Si no fuiste redireccionado a Whatsapp no te preocupes tus boletos ya estan apartados con tus datos.
         Toda tu informacion sera enviada a nuestro correo electronico, nosotros te contactaremos.    </p>
