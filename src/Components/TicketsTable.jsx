b7e5be939983829aaaeb598b99884df2a87aed2a
Albertosl2497 <146301297+Albertosl2497@users.noreply.github.com>
2023-10-12 15:17:31 -0700

    Update TicketsTable.jsx

--- src/Components/TicketsTable.jsx
+++ src/Components/TicketsTable.jsx
@@ -1,6 +1,6 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import { AgGridReact } from "ag-grid-react";
-import "ag-grid-community/styles/ag-grid.css"; // Core grid CSS, always needed
+import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
 import "./ticket.css";
 import { toast } from "react-toastify";
@@ -12,114 +12,54 @@ function TicketTable({ tickets, lotteryNo, setStats, stats }) {
       method: "POST",
       headers: { "Content-Type": "application/json" },
     };
-    let isChanged = false;
-    if (
-      params.colDef.field === "sold" ||
-      params.colDef.field === "availability"
-    ) {
+
+    if (params.colDef.field === "sold") {
       const ticketToUpdate = params.data;
-      let newStatus, newAvailability;
-      if (params.colDef.field === "sold") {
-        let value = ticketToUpdate.sold;
-        fetch(
-          `https://rifasefectivocampotreinta.onrender.com/api/tickets/sold-ticket/${lotteryNo}/${
-            ticketToUpdate.ticketNumber
-          }/${!value}`,
-          requestOptions
-        )
-          .then((response) => response.json())
-          .then((data) => {
-            newStatus = ticketToUpdate.sold ? false : true;
-            newAvailability = ticketToUpdate.availability;
-            isChanged = true;
-            if (isChanged) {
-              const updatedData = [...rowData];
-              const updatedTicket = {
-                ...ticketToUpdate,
-                sold: newStatus,
-                availability: !newStatus,
-              };
-              const rowIndex = updatedData.findIndex((row) => {
-                return row.ticketNumber === ticketToUpdate.ticketNumber;
-              });
+      const newValue = !ticketToUpdate.sold;
 
-              let soldCount = newStatus ? 1 : -1;
+      // Solo si estamos cambiando de "pagado" a "no pagado", también cambiamos la disponibilidad
+      const newAvailability = newValue ? ticketToUpdate.availability : true;
 
-              setStats({
-                soldCount: stats.soldCount + soldCount,
-                bookedCount: stats.bookedCount,
-              });
+      fetch(
+        `https://rifasefectivocampotreinta.onrender.com/api/tickets/sold-ticket/${lotteryNo}/${ticketToUpdate.ticketNumber}/${newValue}`,
+        requestOptions
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          // Actualizamos el estado solo si no hay errores en la solicitud
+          if (data.error) {
+            toast.error(data.error);
+          } else {
+            const updatedData = [...rowData];
+            const updatedTicket = {
+              ...ticketToUpdate,
+              sold: newValue,
+              availability: newAvailability,
+            };
+            const rowIndex = updatedData.findIndex((row) => {
+              return row.ticketNumber === ticketToUpdate.ticketNumber;
+            });
 
-              updatedData[rowIndex] = updatedTicket;
-              setRowData(updatedData);
-            }
+            let soldCount = newValue ? 1 : -1;
+
+            setStats({
+              soldCount: stats.soldCount + soldCount,
+              bookedCount: stats.bookedCount,
+            });
+
+            updatedData[rowIndex] = updatedTicket;
+            setRowData(updatedData);
             toast.success("Estado del boleto actualizado exitosamente");
-          })
-          .catch((error) => {
-            console.error(error);
-            toast.error("Error al actualizar el estado del boleto");
-          });
-      } else {
-        const value = ticketToUpdate.availability;
-        fetch(
-          `https://rifasefectivocampotreinta.onrender.com/api/tickets/claim-ticket/${lotteryNo}/${ticketToUpdate.ticketNumber}/${value}`,
-          requestOptions
-        )
-          .then((response) => response.json())
-          .then((data) => {
-            if (data.message === "Sold Tickets can not be made available") {
-              toast.error(data.message);
-              return;
-            }
-
-            newStatus = ticketToUpdate.sold;
-            newAvailability = ticketToUpdate.availability ? false : true;
-            isChanged = true;
-            if (isChanged) {
-              const updatedData = [...rowData];
-              const updatedTicket = {
-                ...ticketToUpdate,
-                sold: newAvailability ? false : newStatus,
-                availability: newAvailability,
-              };
-
-              let bookedCount = !newAvailability ? 1 : -1;
-
-              setStats({
-                soldCount: stats.soldCount,
-                bookedCount: stats.bookedCount + bookedCount,
-              });
-
-              const rowIndex = updatedData.findIndex((row) => {
-                return row.ticketNumber === ticketToUpdate.ticketNumber;
-              });
-
-              updatedData[rowIndex] = updatedTicket;
-              setRowData(updatedData);
-            }
-            toast.success("Ticket availability updated successfully");
-          })
-          .catch((error) => {
-            console.error(error);
-            toast.error("Error updating ticket availability");
-          });
-      }
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+          toast.error("Error al actualizar el estado del boleto");
+        });
     }
   };
 
   const [rowData, setRowData] = useState([]);
-
-  const [gridApi, setGridApi] = useState(null);
-  const [gridColumnApi, setGridColumnApi] = useState(null);
-
-  const onGridReady = (params) => {
-    setGridApi(params.api);
-    setGridColumnApi(params.columnApi);
-  };
-
-  const onQuickFilterChanged = () => {
-    gridApi.setQuickFilter(document.getElementById("quickFilter").value);
-  };
 
   const columnDefs = [
     {
@@ -182,23 +122,11 @@ function TicketTable({ tickets, lotteryNo, setStats, stats }) {
 
   return (
     <div style={{ width: "100%", marginTop: 20, height: "100%" }}>
-      <input
-        type="text"
-        id="quickFilter"
-        placeholder="Search..."
-        onChange={onQuickFilterChanged}
-        style={{
-          backgroundColor: "black",
-          color: "white",
-          border: "none",
-        }}
-      />
       <div className="ag-theme-alpine-dark">
         <AgGridReact
           rowData={rowData}
           columnDefs={columnDefs}
-          onGridReady={onGridReady}
-          onCellDoubleClicked={onCellDoubleClicked} // add
+          onCellDoubleClicked={onCellDoubleClicked}
           pagination={true}
           paginationPageSize={1000}
           rowSelection={"single"}
