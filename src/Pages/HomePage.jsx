3af7b0692bb1fdf134e952c988e85f4df989e0cb
Albertosl2497 <146301297+Albertosl2497@users.noreply.github.com>
2023-11-06 13:28:46 -0700

    Update HomePage.jsx

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -7,10 +7,10 @@ import "./styles.css";
 function MainPageContent() {
   return (
     <div className="col" style={{ display: 'flex', flexDirection: 'column' }}>
-      <p className="bold" style={{ fontSize: '24px' }}>GRAN EDICIÓN # 15</p>
-      <p className="bold" style={{ fontSize: '24px' }}>ESTE 7 DE NOVIEMBRE</p>
-      <p className="bold" style={{ fontSize: '24px' }}>LLEVATE $5000 PESOS</p>
-      <p style={{ fontSize: '20px' }}>Costo por Boleto $80.00 MXN</p>
+      <p className="bold" style={{ fontSize: '24px' }}>GRAN EDICIÓN # 16</p>
+      <p className="bold" style={{ fontSize: '24px' }}>ESTE 12 DE NOVIEMBRE</p>
+      <p className="bold" style={{ fontSize: '24px' }}>LLEVATE $7000 PESOS</p>
+      <p style={{ fontSize: '20px' }}>Costo por Boleto $100.00 MXN</p>
       <br />
       
       
