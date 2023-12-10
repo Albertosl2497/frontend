2505b683b9cdcc205b3a2868345a9f2b619d90bc
Albertosl2497 <146301297+Albertosl2497@users.noreply.github.com>
2023-12-09 21:19:52 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,26 +18,6 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [lotteryNo, setLotteryNo] = useState(0);
 
-  window.fbAsyncInit = function () {
-  window.FB.init({
-    appId: 'tu_app_id',
-    autoLogAppEvents: true,
-    xfbml: true,
-    version: 'v11.0',
-  });
-};
-
-(function (d, s, id) {
-  var js,
-    fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) {
-    return;
-  }
-  js = d.createElement(s);
-  js.id = id;
-  js.src = 'https://connect.facebook.net/en_US/sdk.js';
-  fjs.parentNode.insertBefore(js, fjs);
-})(document, 'script', 'facebook-jssdk');
   
   useEffect(() => {
     const getTickets = async () => {
