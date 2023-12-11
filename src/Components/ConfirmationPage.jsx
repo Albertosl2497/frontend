cad7f5122b5382eed59e44457019b7124aabd0da
Albertosl2497 <146301297+Albertosl2497@users.noreply.github.com>
2023-12-10 22:56:45 -0700

    Update ConfirmationPage.jsx

--- src/Components/ConfirmationPage.jsx
+++ src/Components/ConfirmationPage.jsx
@@ -1,1 +1,15 @@
+import React from 'react';
+
+function ConfirmationPage({ selectedTickets, totalPrice }) {
+  return (
+    <div>
+      <h2>Confirmación de Compra</h2>
+      <p>Cantidad de boletos: {selectedTickets.length}</p>
+      <p>Costo total: ${totalPrice}</p>
+      {/* Otros detalles de confirmación */}
+    </div>
+  );
+}
+
+export default ConfirmationPage;
 
