8a7e7653c063f7aaba4241b81c96953479fa1fc9
Albertosl2497 <146301297+Albertosl2497@users.noreply.github.com>
2024-02-17 15:23:28 -0700

    Update TicketForm.jsx

--- src/Components/TicketForm.jsx
+++ src/Components/TicketForm.jsx
@@ -143,7 +143,7 @@ function TicketForm({ tickets, loading, lotteryNo, setTickets }) {
 
 
 sendWhatsAppMessage(
-  mobNumber, // Aquí usamos el número de teléfono del cliente
+  `+${phoneNumberCountryCode} ${phoneNumber}`, // Aquí usamos el número de teléfono del cliente
   `HOLA,
   HAS RESERVADO ${selectedTicketCount} BOLETO(S).
   𝘾𝙊𝙉 𝙇𝙊𝙎 𝙉𝙐𝙈𝙀𝙍𝙊𝙎: [ ${selectedTicketNumbers} ].
