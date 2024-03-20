32fb30b6f1f32ce992d87fbb5f999d2008334f26
Albertosl2497 <146301297+Albertosl2497@users.noreply.github.com>
2024-03-19 22:26:06 -0700

    Update TicketForm.jsx

--- src/Components/TicketForm.jsx
+++ src/Components/TicketForm.jsx
@@ -141,8 +141,7 @@ function TicketForm({ tickets, loading, lotteryNo, setTickets }) {
               <hr style={{ border: '1px solid #ccc', marginBottom: '20px' }} />
               <p style={{ color: '#555', marginBottom: '3px', fontSize: '14px',fontWeight: 'bold'}}
                 >𝗛𝗔𝗦 𝗥𝗘𝗦𝗘𝗥𝗩𝗔𝗗𝗢 𝗟𝗢𝗦 𝗡𝗨𝗠𝗘𝗥𝗢𝗦:< br/>
-                [ {selectedTickets.map(ticket => `${ticket} - ${parseInt(ticket) + 5000}`).join(", ")} ]< br/>
-                𝗣𝗔𝗥𝗔 𝗘𝗟 𝗦𝗢𝗥𝗧𝗘𝗢 𝗗𝗘:< br/>
+                [ {selectedTickets.slice(0, 10).map(ticket => `${ticket} - ${parseInt(ticket) + 5000}`).join(", ")} {selectedTickets.length > 10 ? "..." : ""} ]<br />                𝗣𝗔𝗥𝗔 𝗘𝗟 𝗦𝗢𝗥𝗧𝗘𝗢 𝗗𝗘:< br/>
                 $40,000 PESOS EN EFECTIVO. 𝗗𝗘𝗟 𝗗𝗜𝗔: 10 DE MAYO DE 2024.< br/>
                 (𝘼𝘿𝙀𝙈𝘼𝙎 $10,000 𝙀𝙉 𝙀𝙁𝙀𝘾𝙏𝙄𝙑𝙊 𝘿𝙀 𝙋𝙍𝙀𝙎𝙊𝙍𝙏𝙀𝙊 𝙀𝙇 𝘿𝙄𝘼 30 𝘿𝙀 𝘼𝘽𝙍𝙄𝙇)< br/>
                     𝗧𝗘𝗟𝗘𝗙𝗢𝗡𝗢: {mobNumber}.< br/>
@@ -152,7 +151,7 @@ function TicketForm({ tickets, loading, lotteryNo, setTickets }) {
                     {city}, {state}.< br/>
                     𝗣𝗥𝗘𝗖𝗜𝗢 𝗧𝗢𝗧𝗔𝗟: ${totalPrice} PESOS. < br/></p>
                   <p style={{ color: '#555', marginBottom: '3px', fontSize: '10px',fontWeight: 'bold'}}>
-                 Estaras recibiendo confirmacion por Whatsapp en unos momentos. Gracias por participar.😊🌼
+                 Estaras recibiendo confirmacion por Whatsapp en unos momentos. Gracias por participar.😊🌼</p>
             </div>
     
              
