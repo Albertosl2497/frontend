c274b59add709560e3213f8c8d7d1c9f82c988b1
Albertosl2497 <146301297+Albertosl2497@users.noreply.github.com>
2024-04-09 23:35:09 -0700

    Update TicketForm.jsx

--- src/Components/TicketForm.jsx
+++ src/Components/TicketForm.jsx
@@ -143,13 +143,13 @@ const totalTickets = selectedTicketCount;
               <h3 style={{ color: '#333', marginBottom: '10px', fontSize: '18px', fontWeight: 'bold', textAlign: 'center' }}>"REGISTRO EXITOSO"</h3>
               <hr style={{ border: '1px solid #ccc', marginBottom: '20px' }} />
               <p style={{ color: '#555', marginBottom: '3px', fontSize: '14px',fontWeight: 'bold'}}
-                >HOLA, HAS RESERVADO ${totalTickets} BOLETO(S).
-                𝘾𝙊𝙉 𝙇𝙊𝙎 𝙉𝙐𝙈𝙀𝙍𝙊𝙎:[${selectedTicketNumbers}].
+                >HOLA, HAS RESERVADO {totalTickets} BOLETO(S).
+                𝘾𝙊𝙉 𝙇𝙊𝙎 𝙉𝙐𝙈𝙀𝙍𝙊𝙎:[{selectedTicketNumbers}].
                 OPORTUNIDADES ADICIONALES:
-                [ ${selectedTicketNumbersWithPairs.join(', ')} ].
+                [ {selectedTicketNumbersWithPairs.join(', ')} ].
                 𝗣𝗔𝗥𝗔 𝗘𝗟 𝗦𝗢𝗥𝗧𝗘𝗢 𝗗𝗘:< br/>
                 $15,000 PESOS EN EFECTIVO.< br/>
-                𝗗𝗘𝗟 𝗗𝗜𝗔: 09 DE ABRIL DE 2024.< br/>
+                𝗗𝗘𝗟 𝗗𝗜𝗔: 21 DE ABRIL DE 2024.< br/>
                 𝗡𝗢𝗠𝗕𝗥𝗘:< br/>
                 {fullName}.< br/>
                 
@@ -166,7 +166,7 @@ const totalTickets = selectedTicketCount;
       𝘾𝙊𝙉 𝙇𝙊𝙎 𝙉𝙐𝙈𝙀𝙍𝙊𝙎:[${selectedTicketNumbers}].
       OPORTUNIDADES ADICIONALES:
       [ ${selectedTicketNumbersWithPairs.join(', ')} ].
-      𝙋𝘼𝙍𝘼 𝙀𝙇 𝙎𝙊𝙍𝙏𝙀𝙊 𝘿𝙀: $15,000 EN EFECTIVO. DEL DIA 9 DE ABRIL DE 2024.
+      𝙋𝘼𝙍𝘼 𝙀𝙇 𝙎𝙊𝙍𝙏𝙀𝙊 𝘿𝙀: $15,000 EN EFECTIVO. DEL DIA 21 DE ABRIL DE 2024.
       
       𝙀𝙇 𝙋𝙍𝙀𝘾𝙄𝙊 𝘼 𝙋𝘼𝙂𝘼𝙍 𝙀𝙎:
       $${totalPrice} PESOS.
