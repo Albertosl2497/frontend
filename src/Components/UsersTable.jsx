d137c06a4c0136972edf83255ab6ebb6ea73a043
Albertosl2497 <146301297+Albertosl2497@users.noreply.github.com>
2024-06-02 09:27:53 -0700

    Actualizar UsersTable.jsx

--- src/Components/UsersTable.jsx
+++ src/Components/UsersTable.jsx
@@ -151,7 +151,7 @@ const [confirmationSentEmails, setConfirmationSentEmails] = useState([]);
   const estado = userData.user.state;
 
     
-  const message = `HOLA BUENAS TARDES SOLO PARA RECORDAR QUE EL DIA DE HOY SE LLEVARA ACABO LA RIFA DE LOS $15,000 PESOS💸
+  const message = `HOLA BUENOS DIAS SOLO PARA RECORDAR QUE EL DIA DE HOY SE LLEVARA ACABO LA RIFA DE LOS $15,000 PESOS💸
   𝗘𝗦𝗧𝗔𝗥𝗘𝗠𝗢𝗦 𝗥𝗘𝗖𝗜𝗕𝗜𝗘𝗡𝗗𝗢 𝗟𝗢𝗦 𝗣𝗔𝗚𝗢𝗦 𝗛𝗔𝗦𝗧𝗔 𝗟𝗔𝗦 2PM 𝗗𝗘𝗟 𝗗𝗜𝗔 𝗗𝗘 𝗛𝗢𝗬.
   
   TENEMOS APARTADO ${ticketCount} BOLETO(S) A NOMBRE DE: ${fullName}.
@@ -176,7 +176,7 @@ const [confirmationSentEmails, setConfirmationSentEmails] = useState([]);
   const estado = userData.user.state;
 
     
-  const message = `HOLA BUENAS NOCHES SOLO PARA INFORMAR QUE LA HORA LIMITE DE PAGO SERA A LAS 12PM EN MEDIO DIA.
+  const message = `HOLA BUENOS DIAS SOLO PARA INFORMAR QUE LA HORA LIMITE DE PAGO SERA A LAS 12PM EN MEDIO DIA.
   SI GUSTA QUE ESPEREMOS UN POCO MAS NOS CONFIRMA PORFA. GRACIAS😊🌼`
  const whatsappUrl = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
   window.open(whatsappUrl, "_blank");
