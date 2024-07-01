bee4156819deb03210936551674826187baf3f6e
Albertosl2497 <146301297+Albertosl2497@users.noreply.github.com>
2024-06-30 23:58:29 -0700

    Update TicketForm.jsx

--- src/Components/TicketForm.jsx
+++ src/Components/TicketForm.jsx
@@ -384,7 +384,7 @@ const totalTickets = selectedTicketCount;
           </div>
 
           {/* Ciudad */}
-          <label className="bold-label">Ciudad</label>
+          
           <div className="form-row">
             <input
               type="hidden"
