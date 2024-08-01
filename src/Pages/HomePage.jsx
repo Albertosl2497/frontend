b9d0ab0870a3d88ae392cbf9cba2e66d7c91f841
Albertosl2497 <146301297+Albertosl2497@users.noreply.github.com>
2024-07-31 17:51:45 -0700

    Update HomePage.jsx

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -8,7 +8,7 @@ function MainPageContent() {
   return (
     <div className="col" style={{ display: 'flex', flexDirection: 'column' }}>
       <p className="bold" style={{ fontSize: '24px' }}>GRAN EDICIÓN # 61</p>
-      <p className="bold" style={{ fontSize: '24px' }}>ESTE 09 DE AGOSTO</p>
+      <p className="bold" style={{ fontSize: '24px' }}>ESTE 16 DE AGOSTO</p>
       <p className="bold" style={{ fontSize: '24px' }}>LLEVATE $15,000 PESOS</p>
       <p style={{ fontSize: '20px' }}>(SOLO 250 PARTICIPANTES)</p>
       <p style={{ fontSize: '20px' }}>Cada boleto con 4 oportunidades</p>
