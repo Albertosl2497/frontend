e93624b15ead78392ed4f5ec37a85645e7023db2
Albertosl2497 <146301297+Albertosl2497@users.noreply.github.com>
2024-09-23 12:38:07 -0700

    Update TicketsTable.jsx

--- src/Components/TicketsTable.jsx
+++ src/Components/TicketsTable.jsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { AgGridReact } from "ag-grid-react";
-import "ag-grid-community/styles/ag-grid.css"; 
+import "ag-grid-community/styles/ag-grid.css"; // Core grid CSS, always needed
 import "ag-grid-community/styles/ag-theme-alpine.css";
 import "./ticket.css";
 import { toast } from "react-toastify";
@@ -8,37 +8,39 @@ import "react-toastify/dist/ReactToastify.css";
 
 // Definimos la función copyUserName fuera del componente
 const copyUserName = (userName) => {
+  // Dividir el nombre de usuario y el correo electrónico
   const [name, email] = userName.split(' (');
-  const uppercaseName = name.trim().toUpperCase();
-  navigator.clipboard.writeText(uppercaseName);
+  const uppercaseName = name.trim().toUpperCase(); // Convertir el nombre a mayúsculas
+  navigator.clipboard.writeText(uppercaseName); // Copiar el nombre de usuario en mayúsculas
   toast.success("Nombre de usuario copiado exitosamente");
 };
 
-function TicketTable({ tickets, lotteryNo, setStats, stats, updateUserPaymentMethod }) {
-  const [rowData, setRowData] = useState([]);
-  const [gridApi, setGridApi] = useState(null); // Definido aquí
-  const [gridColumnApi, setGridColumnApi] = useState(null); // Definido aquí
-
+function TicketTable({ tickets, lotteryNo, setStats, stats }) {
   const onCellDoubleClicked = (params) => {
     const requestOptions = {
       method: "POST",
       headers: { "Content-Type": "application/json" },
     };
     let isChanged = false;
-
-    if (params.colDef.field === "sold" || params.colDef.field === "availability") {
+    if (
+      params.colDef.field === "sold" ||
+      params.colDef.field === "availability"
+    ) {
       const ticketToUpdate = params.data;
       let newStatus, newAvailability;
-
       if (params.colDef.field === "sold") {
         let value = ticketToUpdate.sold;
-        fetch(`https://rifasefectivocampotreinta.onrender.com/api/tickets/sold-ticket/${lotteryNo}/${ticketToUpdate.ticketNumber}/${!value}`, requestOptions)
+        fetch(
+          https://rifasefectivocampotreinta.onrender.com/api/tickets/sold-ticket/${lotteryNo}/${
+            ticketToUpdate.ticketNumber
+          }/${!value},
+          requestOptions
+        )
           .then((response) => response.json())
           .then((data) => {
-            newStatus = !value;
+            newStatus = ticketToUpdate.sold ? false : true;
             newAvailability = ticketToUpdate.availability;
             isChanged = true;
-
             if (isChanged) {
               const updatedData = [...rowData];
               const updatedTicket = {
@@ -46,8 +48,10 @@ function TicketTable({ tickets, lotteryNo, setStats, stats, updateUserPaymentMet
                 sold: newStatus,
                 availability: !newStatus,
               };
+              const rowIndex = updatedData.findIndex((row) => {
+                return row.ticketNumber === ticketToUpdate.ticketNumber;
+              });
 
-              const rowIndex = updatedData.findIndex((row) => row.ticketNumber === ticketToUpdate.ticketNumber);
               let soldCount = newStatus ? 1 : -1;
 
               setStats({
@@ -57,24 +61,65 @@ function TicketTable({ tickets, lotteryNo, setStats, stats, updateUserPaymentMet
 
               updatedData[rowIndex] = updatedTicket;
               setRowData(updatedData);
-              
-              // Actualiza el método de pago en UsersTable
-              if (newStatus) {
-                updateUserPaymentMethod(ticketToUpdate.user.email, ticketToUpdate.paymentMethod);
-              }
-
-              toast.success("Estado del boleto actualizado exitosamente");
             }
+            toast.success("Estado del boleto actualizado exitosamente");
           })
           .catch((error) => {
             console.error(error);
             toast.error("Error al actualizar el estado del boleto");
           });
       } else {
-        // Handle availability toggle
+        const value = ticketToUpdate.availability;
+        fetch(
+          https://rifasefectivocampotreinta.onrender.com/api/tickets/claim-ticket/${lotteryNo}/${ticketToUpdate.ticketNumber}/${value},
+          requestOptions
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            if (data.message === "Sold Tickets can not be made available") {
+              toast.error(data.message);
+              return;
+            }
+
+            newStatus = ticketToUpdate.sold;
+            newAvailability = ticketToUpdate.availability ? false : true;
+            isChanged = true;
+            if (isChanged) {
+              const updatedData = [...rowData];
+              const updatedTicket = {
+                ...ticketToUpdate,
+                sold: newAvailability ? false : newStatus,
+                availability: newAvailability,
+              };
+
+              let bookedCount = !newAvailability ? 1 : -1;
+
+              setStats({
+                soldCount: stats.soldCount,
+                bookedCount: stats.bookedCount + bookedCount,
+              });
+
+              const rowIndex = updatedData.findIndex((row) => {
+                return row.ticketNumber === ticketToUpdate.ticketNumber;
+              });
+
+              updatedData[rowIndex] = updatedTicket;
+              setRowData(updatedData);
+            }
+            toast.success("Ticket availability updated successfully");
+          })
+          .catch((error) => {
+            console.error(error);
+            toast.error("Error updating ticket availability");
+          });
       }
     }
   };
+
+  const [rowData, setRowData] = useState([]);
+
+  const [gridApi, setGridApi] = useState(null);
+  const [gridColumnApi, setGridColumnApi] = useState(null);
 
   const onGridReady = (params) => {
     setGridApi(params.api);
@@ -87,12 +132,12 @@ function TicketTable({ tickets, lotteryNo, setStats, stats, updateUserPaymentMet
 
   const columnDefs = [
     {
-      headerName: "Boletos #",
+  headerName: "Boletos #",
       field: "ticketNumber",
       sortable: true,
       resizable: true,
       width: 80,
-    },
+       },
     {
       headerName: "Propietario",
       field: "user",
@@ -100,6 +145,9 @@ function TicketTable({ tickets, lotteryNo, setStats, stats, updateUserPaymentMet
       resizable: true,
       sortable: true,
       width: 110,
+      cellRenderer: function (params) {
+        return params.value;
+      },
     },
     {
       headerName: "Estado",
@@ -109,10 +157,16 @@ function TicketTable({ tickets, lotteryNo, setStats, stats, updateUserPaymentMet
       resizable: true,
       width: 130,
       cellClassRules: {
-        "cell-value-green": (params) => !params.value,
-        "cell-value-red": (params) => params.value,
+        "cell-value-green": function (params) {
+          return !params.value;
+        },
+        "cell-value-red": function (params) {
+          return params.value;
+        },
       },
-      cellRenderer: (params) => (params.value ? "Pagado" : "No Pagado"),
+      cellRenderer: function (params) {
+        return params.value ? "Pagado" : "No Pagado";
+      },
     },
     {
       headerName: "Disponibilidad",
@@ -122,21 +176,29 @@ function TicketTable({ tickets, lotteryNo, setStats, stats, updateUserPaymentMet
       resizable: true,
       width: 130,
       cellClassRules: {
-        "cell-value-green": (params) => !params.value,
-        "cell-value-red": (params) => params.value,
+        "cell-value-green": function (params) {
+          return !params.value;
+        },
+        "cell-value-red": function (params) {
+          return params.value;
+        },
       },
-      cellRenderer: (params) => (params.value ? "Disponible" : "No Disponible"),
+      cellRenderer: function (params) {
+        return params.value ? "Disponible" : "No Disponible";
+      },
     },
+
     {
       headerName: "Copiar Usuario",
       field: "user",
       width: 110,
       cellRendererFramework: (params) => (
-        <button onClick={() => copyUserName(params.value)} style={{ backgroundColor: "blue", color: "white", border: "none", padding: "10px 20px", borderRadius: "5px", cursor: "pointer" }}>
-          Copiar
-        </button>
+        <button onClick={() => copyUserName(params.value)}
+        style={{ backgroundColor: "blue", color: "white", border: "none", padding: "10px 20px", borderRadius: "5px", cursor: "pointer" }}
+          >Copiar</button>
       ),
     },
+    
   ];
 
   useEffect(() => {
@@ -150,14 +212,18 @@ function TicketTable({ tickets, lotteryNo, setStats, stats, updateUserPaymentMet
         id="quickFilter"
         placeholder="Search..."
         onChange={onQuickFilterChanged}
-        style={{ backgroundColor: "black", color: "white", border: "none" }}
+        style={{
+          backgroundColor: "black",
+          color: "white",
+          border: "none",
+        }}
       />
       <div className="ag-theme-alpine-dark">
         <AgGridReact
           rowData={rowData}
           columnDefs={columnDefs}
           onGridReady={onGridReady}
-          onCellDoubleClicked={onCellDoubleClicked}
+          onCellDoubleClicked={onCellDoubleClicked} // add
           pagination={true}
           paginationPageSize={1000}
           rowSelection={"single"}
