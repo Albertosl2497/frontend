72b2b4682f4189360f48752d9500cc9eea245974
Albertosl2497 <146301297+Albertosl2497@users.noreply.github.com>
2024-09-22 22:04:14 -0700

    Update HomePage.jsx

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -7,12 +7,10 @@ import "./styles.css";
 function MainPageContent() {
   return (
     <div className="col" style={{ display: 'flex', flexDirection: 'column' }}>
-      <p className="bold" style={{ fontSize: '24px' }}>GRAN EDICIÓN # 61</p>
-      <p className="bold" style={{ fontSize: '24px' }}>ESTE 16 DE AGOSTO</p>
-      <p className="bold" style={{ fontSize: '24px' }}>LLEVATE $15,000 PESOS</p>
-      <p style={{ fontSize: '20px' }}>(SOLO 250 PARTICIPANTES)</p>
-      <p style={{ fontSize: '20px' }}>Cada boleto con 4 oportunidades</p>
-      <p style={{ fontSize: '20px' }}>Costo por Boleto $100.00 MXN</p>
+      <p className="bold" style={{ fontSize: '24px' }}>GRAN EDICIÓN # 62</p>
+      <p className="bold" style={{ fontSize: '24px' }}>ESTE 24 DE SEPTIEMBRE</p>
+      <p className="bold" style={{ fontSize: '24px' }}>LLEVATE $3000 PESOS</p>
+      <p style={{ fontSize: '20px' }}>Costo por Boleto $50.00 MXN</p>
       <br />
       
       
