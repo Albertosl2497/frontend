1c1eb6a3fce90723ba7a18a94014ad8ab90f30cb
Albertosl2497 <146301297+Albertosl2497@users.noreply.github.com>
2024-10-23 12:08:27 -0700

    Update UsersTable.jsx

--- src/Components/UsersTable.jsx
+++ src/Components/UsersTable.jsx
@@ -151,8 +151,8 @@ const [confirmationSentEmails, setConfirmationSentEmails] = useState([]);
   const estado = userData.user.state;
 
     
-  const message = `HOLA BUENOS DIAS SOLO PARA RECORDAR QUE EL DIA DE HOY SE LLEVARA ACABO LA RIFA DE LOS $15,000 PESOS💸
-  𝗘𝗦𝗧𝗔𝗥𝗘𝗠𝗢𝗦 𝗥𝗘𝗖𝗜𝗕𝗜𝗘𝗡𝗗𝗢 𝗟𝗢𝗦 𝗣𝗔𝗚𝗢𝗦 𝗛𝗔𝗦𝗧𝗔 𝗟𝗔𝗦 𝟯𝗣𝗠 𝗗𝗘𝗟 𝗗𝗜𝗔 𝗗𝗘 𝗛𝗢𝗬.
+  const message = `HOLA BUENOS DIAS SOLO PARA CONFIRMAR SU PARTICIPACION EN LA RIFA DE LOS $15,000 PESOS💸
+  DEL DIA 31 DE OCTUBRE. 𝗟𝗔 𝗙𝗘𝗖𝗛𝗔 𝗟𝗜𝗠𝗜𝗧𝗘 𝗗𝗘 𝗣𝗔𝗚𝗢 𝗘𝗦 𝗘𝗟 𝗗𝗜𝗔 𝗠𝗜𝗘𝗥𝗖𝗢𝗟𝗘𝗦 𝟯𝟬 𝗗𝗘 𝗢𝗖𝗧𝗨𝗕𝗥𝗘. GRACIAS.☺️🌸
   
   TENEMOS APARTADO ${ticketCount} BOLETO(S) A NOMBRE DE: ${fullName}.
   CON UN PRECIO DE: $${totalPrice} PESOS.
