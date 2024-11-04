3548b6571bb2defa1e3ed12cf56bd7008c2e4ab8
Albertosl2497 <146301297+Albertosl2497@users.noreply.github.com>
2024-11-04 12:24:12 -0700

    Update HomePage.jsx

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -7,8 +7,8 @@ import "./styles.css";
 function MainPageContent() {
   return (
     <div className="col" style={{ display: 'flex', flexDirection: 'column' }}>
-      <p className="bold" style={{ fontSize: '24px' }}>GRAN EDICIÓN # 62</p>
-      <p className="bold" style={{ fontSize: '24px' }}>ESTE 31 DE OCTUBRE</p>
+      <p className="bold" style={{ fontSize: '24px' }}>GRAN EDICIÓN # 63</p>
+      <p className="bold" style={{ fontSize: '24px' }}>ESTE 09 DE NOVIEMBRE</p>
       <p className="bold" style={{ fontSize: '24px' }}>LLEVATE $15,000 PESOS</p>
       <p style={{ fontSize: '20px' }}>(SOLO 250 PARTICIPANTES)</p>
       <p style={{ fontSize: '20px' }}>Cada boleto con 4 oportunidades</p>
