a6591023b3f625a7e8d5ea8ed18b057ab46be9f0
Albertosl2497 <146301297+Albertosl2497@users.noreply.github.com>
2024-11-22 12:10:05 -0700

    Update HomePage.jsx

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -7,10 +7,12 @@ import "./styles.css";
 function MainPageContent() {
   return (
     <div className="col" style={{ display: 'flex', flexDirection: 'column' }}>
-      <p className="bold" style={{ fontSize: '24px' }}>EDICIÓN EXPRESS ⚡️ </p>
-      <p className="bold" style={{ fontSize: '24px' }}>ESTE 24 DE NOVIEMBRE</p>
-      <p className="bold" style={{ fontSize: '24px' }}>LLEVATE $3000 PESOS</p>
-      <p style={{ fontSize: '20px' }}>Costo por Boleto $50.00 MXN</p>
+      <p className="bold" style={{ fontSize: '24px' }}>GRAN EDICIÓN # 64</p>
+      <p className="bold" style={{ fontSize: '24px' }}>ESTE 03 DE DICIEMBRE</p>
+      <p className="bold" style={{ fontSize: '24px' }}>LLEVATE $15,000 PESOS</p>
+      <p style={{ fontSize: '20px' }}>(SOLO 250 PARTICIPANTES)</p>
+      <p style={{ fontSize: '20px' }}>Cada boleto con 4 oportunidades</p>
+      <p style={{ fontSize: '20px' }}>Costo por Boleto $100.00 MXN</p>
       <br />
       
       
