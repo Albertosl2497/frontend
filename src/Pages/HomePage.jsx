c8c870b61716939cd8e47289000ba967a50506e5
Albertosl2497 <146301297+Albertosl2497@users.noreply.github.com>
2025-04-02 22:24:16 -0700

    Update HomePage.jsx

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -7,10 +7,12 @@ import "./styles.css";
 function MainPageContent() {
   return (
     <div className="col" style={{ display: 'flex', flexDirection: 'column' }}>
-      <p className="bold" style={{ fontSize: '24px' }}>EDICIÓN EXPRESS⚡️</p>
-      <p className="bold" style={{ fontSize: '24px' }}>02 DE ABRIL 2025</p>
-      <p className="bold" style={{ fontSize: '24px' }}>LLEVATE $3000 PESOS</p>
-      <p style={{ fontSize: '20px' }}>Costo por Boleto $50.00 MXN</p>
+      <p className="bold" style={{ fontSize: '24px' }}>GRAN EDICIÓN # 68</p>
+      <p className="bold" style={{ fontSize: '24px' }}>ESTE 13 DE ABRIL</p>
+      <p className="bold" style={{ fontSize: '24px' }}>LLEVATE $15,000 PESOS</p>
+      <p style={{ fontSize: '20px' }}>(SOLO 250 PARTICIPANTES)</p>
+      <p style={{ fontSize: '20px' }}>Cada boleto con 4 oportunidades</p>
+      <p style={{ fontSize: '20px' }}>Costo por Boleto $100.00 MXN</p>
       <br />
       
       
