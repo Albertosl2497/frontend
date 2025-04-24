73d568683ea31a7c5340d139bfdc20a20ae97533
Albertosl2497 <146301297+Albertosl2497@users.noreply.github.com>
2025-04-24 10:20:38 -0700

    Update TicketForm.jsx

--- src/Components/TicketForm.jsx
+++ src/Components/TicketForm.jsx
@@ -46,7 +46,7 @@ function TicketForm({ tickets, loading, lotteryNo, setTickets }) {
 
   const selectedTicketCount = selectedTickets.length;
 const totalTickets = selectedTicketCount;
-  const ticketPrice = 50; // Precio de cada boleto en pesos
+  const ticketPrice = 100; // Precio de cada boleto en pesos
   const totalPrice = selectedTicketCount * ticketPrice; // Precio total en pesos
   const selectedTicketNumbers = selectedTickets.join(", ");
 
@@ -148,9 +148,11 @@ const totalTickets = selectedTicketCount;
               <p style={{ color: '#555', marginBottom: '3px', fontSize: '14px',fontWeight: 'bold'}}
                 >HOLA, HAS RESERVADO {totalTickets} BOLETO(S).
                 𝘾𝙊𝙉 𝙇𝙊𝙎 𝙉𝙐𝙈𝙀𝙍𝙊𝙎:[{selectedTicketNumbers}].
+                OPORTUNIDADES ADICIONALES:
+                [ {selectedTicketNumbersWithPairs.join(', ')} ].
                 𝗣𝗔𝗥𝗔 𝗘𝗟 𝗦𝗢𝗥𝗧𝗘𝗢 𝗗𝗘:< br/>
-                $3000 PESOS EN EFECTIVO.< br/>
-                𝗗𝗘𝗟 𝗗𝗜𝗔: 23 DE ABRIL DE 2025.< br/>
+                $15,000 PESOS EN EFECTIVO.< br/>
+                𝗗𝗘𝗟 𝗗𝗜𝗔: 01 DE MAYO DE 2025.< br/>
                 𝗡𝗢𝗠𝗕𝗥𝗘:< br/>
                 {fullName}.< br/>
                 
@@ -165,7 +167,9 @@ const totalTickets = selectedTicketCount;
       `52${phoneNumber}`, 
       `HOLA, HAS RESERVADO ${totalTickets} BOLETO(S).
       𝘾𝙊𝙉 𝙇𝙊𝙎 𝙉𝙐𝙈𝙀𝙍𝙊𝙎:[${selectedTicketNumbers}].
-      𝙋𝘼𝙍𝘼 𝙀𝙇 𝙎𝙊𝙍𝙏𝙀𝙊 𝘿𝙀: $3000 EN EFECTIVO. DEL DIA 23 DE ABRIL DE 2025.
+      OPORTUNIDADES ADICIONALES:
+      [ ${selectedTicketNumbersWithPairs.join(', ')} ].
+      𝙋𝘼𝙍𝘼 𝙀𝙇 𝙎𝙊𝙍𝙏𝙀𝙊 𝘿𝙀: $15,000 EN EFECTIVO. DEL DIA 01 DE MAYO DE 2025.
       
       𝘼 𝙉𝙊𝙈𝘽𝙍𝙀 𝘿𝙀: ${fullName}.
       𝙀𝙇 𝙋𝙍𝙀𝘾𝙄𝙊 𝘼 𝙋𝘼𝙂𝘼𝙍 𝙀𝙎: $${totalPrice} PESOS.
@@ -441,7 +445,10 @@ const totalTickets = selectedTicketCount;
         }}
       >
         {ticket} <AiOutlineDelete style={{ fontWeight: 900 }} />
-        
+        {/* Agregar los 3 números adicionales */}
+        {[250, 500, 750].map((additionalNumber) => (
+          <span key={additionalNumber}>{parseInt(ticket) + additionalNumber}</span>
+        ))}
       </div>
     ))}
 </div>
@@ -523,4 +530,3 @@ const totalTickets = selectedTicketCount;
 
 export default TicketForm;
 <ToastContainer position="top-center" autoClose={5000} />;
-
