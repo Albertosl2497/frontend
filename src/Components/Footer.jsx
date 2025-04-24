3d82824f427d701dd7d2e6b11b0f00ad39064ead
Albertosl2497 <146301297+Albertosl2497@users.noreply.github.com>
2025-04-24 14:13:51 -0700

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -9,44 +9,19 @@ function Footer() {
             <h1 className="white bold">METODOS DE PAGO:</h1>
             <div className="col" style={{ display: 'flex', flexDirection: 'column' }}>
               
+              
               <div className="col">
-                <h2 className="white bold">TRANSFERENCIAS 1:</h2>
-                <p className="col bold" style={{ fontSize: '20px' }}>CUENTA: 64-6760-1464-0398-4695</p>
-                <p className="col bold" style={{ fontSize: '20px' }}>BANCO: STP</p>
-                <p className="col bold" style={{ fontSize: '20px' }}>NOMBRE: MARIA RUIZ BORQUEZ</p>
-              </div>
-              <div className="col">
-                <h2 className="col white bold">TRANSFERENCIAS 2:</h2>
-                <p className="col bold" style={{ fontSize: '20px' }}>CUENTA: 64-6760-1464-0337-9116</p>
-                <p className="col bold" style={{ fontSize: '20px' }}>BANCO: STP</p>
-                <p className="col bold" style={{ fontSize: '20px' }}>NOMBRE: MARTIN SANCHEZ LEYVA</p>
-              </div>
-              <div className="col">
-                <h2 className="white bold">TRANSFERENCIAS 3:</h2>
-                <p className="col bold" style={{ fontSize: '20px' }}>CUENTA: 64-6760-1464-0488-2738</p>
-                <p className="col bold" style={{ fontSize: '20px' }}>BANCO: STP</p>
+                <h2 className="white bold">TRANSFERENCIAS:</h2>
+                <p className="col bold" style={{ fontSize: '20px' }}>CUENTA: 72-8969-0001-0756-7678</p>
+                <p className="col bold" style={{ fontSize: '20px' }}>BANCO: Spin By Oxxo</p>
                 <p className="col bold" style={{ fontSize: '20px' }}>NOMBRE: ALVARO RUIZ MURRIETA</p>
               </div>
               <div className="col">
-                <h2 className=" col white bold">DEPOSITOS EN OXXO 1:</h2>
+                <h2 className=" col white bold">DEPOSITOS EN OXXO:</h2>
                 <p className="col bold" style={{ fontSize: '20px' }}>CUENTA: 2242-1707-6033-2708</p>
                 
-              </div>
-              <div className="col">
-                <h2 className="col white bold">DEPOSITOS EN OXXO 2:</h2>
-                <p className="col bold" style={{ fontSize: '20px' }}>CUENTA: 2242-1707-6018-2351</p>
-                
-              </div>
-              <div className="col">
-                <h2 className=" col white bold">DEPOSITOS EN OXXO 3:</h2>
-                <p className="col bold" style={{ fontSize: '20px' }}>CUENTA: 2242-1707-6024-2905</p>
-                
-              </div>
-              <div className="col">
-                <h2 className=" col white bold">DEPOSITOS EN OXXO 3:</h2>
-                <p className="col bold" style={{ fontSize: '20px' }}>CUENTA: 2242-1707-6033-2708</p>
-                
-              </div>
+              
+             
             </div>
           </div>
         </div>
