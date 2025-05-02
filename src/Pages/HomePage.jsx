1b8fca118f62703e576e2b8774297e7f745c0aa6
Albertosl2497 <146301297+Albertosl2497@users.noreply.github.com>
2025-05-02 10:46:21 -0700

    Update HomePage.jsx

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -8,7 +8,7 @@ function MainPageContent() {
   return (
     <div className="col" style={{ display: 'flex', flexDirection: 'column' }}>
       <p className="bold" style={{ fontSize: '24px' }}>GRAN EDICIÓN # 70</p>
-      <p className="bold" style={{ fontSize: '24px' }}>ESTE 16 DE MAYO</p>
+      <p className="bold" style={{ fontSize: '24px' }}>ESTE 18 DE MAYO</p>
       <p className="bold" style={{ fontSize: '24px' }}>LLEVATE $15,000 PESOS</p>
       <p style={{ fontSize: '20px' }}>(SOLO 250 PARTICIPANTES)</p>
       <p style={{ fontSize: '20px' }}>Cada boleto con 4 oportunidades</p>
