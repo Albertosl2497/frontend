3c5f3aadfcf1598e7d69328f8f05f0d5e42b5056
Albertosl2497 <146301297+Albertosl2497@users.noreply.github.com>
2025-06-01 14:55:11 -0700

    Update UsersTable.jsx

--- src/Components/UsersTable.jsx
+++ src/Components/UsersTable.jsx
@@ -183,16 +183,7 @@ const [confirmationSentEmails, setConfirmationSentEmails] = useState([]);
   const estado = userData.user.state;
 
     
-  const message = `HOLA BUENAS TARDES LE RECORDAMOS QUE EL DIA DE HOY SE LLEVARA ACABO LA RIFA DE LOS $15,000 PESOS💸
-  𝗘𝗦𝗧𝗔𝗥𝗘𝗠𝗢𝗦 𝗥𝗘𝗖𝗜𝗕𝗜𝗘𝗡𝗗𝗢 𝗟𝗢𝗦 𝗣𝗔𝗚𝗢𝗦 𝗛𝗔𝗦𝗧𝗔 𝗟𝗔𝗦 03:00𝗣𝗠 𝗗𝗘𝗟 𝗗𝗜𝗔 𝗗𝗘 𝗛𝗢𝗬.
-Si gusta que esperemos un poco mas nos confirma por favor. Gracias ☺️✨️
-  
-  TENEMOS APARTADO ${ticketCount} BOLETO(S) A NOMBRE DE: ${fullName}.
-  CON UN PRECIO DE: $${totalPrice} PESOS.
-  
-  TUS NUMEROS A PARTICIPAR SON:
-  [ ${bookedTickets.join(", ")} ][ ${additionalNumbers} ].
-  METODOS DE PAGO AQUÌ 👉🏼: https://60s.my.canva.site/cuentas `
+  const message = ` Buenas tardes a las 4pm estaremos pasando a cobrar denuevo. *Estara en su casa? para llegar de pasadita?* 🌸`
  const whatsappUrl = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
   window.open(whatsappUrl, "_blank");
  
