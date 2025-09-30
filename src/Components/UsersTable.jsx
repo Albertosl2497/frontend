31ee5d12972a91c726f6f5d5e80cd8b11a2300c2
Albertosl2497 <146301297+Albertosl2497@users.noreply.github.com>
2025-09-30 12:11:32 -0700

    Update UsersTable.jsx

--- src/Components/UsersTable.jsx
+++ src/Components/UsersTable.jsx
@@ -211,7 +211,7 @@ A NOMBRE DE: ${fullName}.
   const estado = userData.user.state;
 
     
-  const message = `Hola buen dia a las 12:30 estaremos pasado a cobrar lo de la rifa del dia hoy. *Estara en su casa? para llegar de pasadita* ✨☺️
+  const message = `Hola buen dia a las 1:00 estaremos pasado a cobrar lo de la rifa del dia hoy. *Estara en su casa? para llegar de pasadita* ✨☺️
 `
  const whatsappUrl = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
   window.open(whatsappUrl, "_blank");
