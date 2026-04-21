afeff74e64504145d22b411875fab0941237357d
Albertosl2497 <146301297+Albertosl2497@users.noreply.github.com>
2026-04-20 23:54:07 -0700

    Actualizar TicketsTable.jsx

--- src/Components/TicketsTable.jsx
+++ src/Components/TicketsTable.jsx
@@ -206,11 +206,11 @@ function TicketTable({ tickets, lotteryNo, setStats, stats }) {
           <div class="header">
             <h1>RIFAS EFECTIVO CAMPO TREINTA</h1>
             <p>WHATSAPP: 6441382876</p>
-            <p>RIFA DE $15,000 PESOS ESTE 19 DE ABRIL 2026</p>
+            <p>RIFA DE $15,000 PESOS ESTE 26 DE ABRIL 2026</p>
           </div>
 
           <div class="page">
-            <h2 class="table-title">GANA $15,000 PESOS ESTE 19 DE ABRIL 2026 - PARTICIPA POR $100 CON 4 OPORTUNIDADES</h2>
+            <h2 class="table-title">GANA $15,000 PESOS ESTE 26 DE ABRIL 2026 - PARTICIPA POR $100 CON 4 OPORTUNIDADES</h2>
             <div class="split">
               <div class="col">${renderBlock(0, 49)}</div>
               <div class="col">${renderBlock(50, 99)}</div>
@@ -218,7 +218,7 @@ function TicketTable({ tickets, lotteryNo, setStats, stats }) {
           </div>
 
           <div class="page">
-            <h2 class="table-title">GANA $15,000 PESOS ESTE 19 DE ABRIL 2026 - PARTICIPA POR $100 CON 4 OPORTUNIDADES</h2>
+            <h2 class="table-title">GANA $15,000 PESOS ESTE 26 DE ABRIL 2026 - PARTICIPA POR $100 CON 4 OPORTUNIDADES</h2>
             <div class="split">
               <div class="col">${renderBlock(100, 149)}</div>
               <div class="col">${renderBlock(150, 199)}</div>
