0cd36b6d31f553ef88c9f82f8c2d39ccfea898e3
Albertosl2497 <146301297+Albertosl2497@users.noreply.github.com>
2026-04-27 08:36:06 -0700

    Actualizar HomePage.jsx

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -7,8 +7,8 @@ import "./styles.css";
 function MainPageContent() {
   return (
     <div className="col" style={{ display: 'flex', flexDirection: 'column' }}>
-      <p className="bold" style={{ fontSize: '24px' }}>GRAN EDICIÓN # 96</p>
-      <p className="bold" style={{ fontSize: '24px' }}>ESTE 26 DE ABRIL DE 2026</p>
+      <p className="bold" style={{ fontSize: '24px' }}>GRAN EDICIÓN # 97</p>
+      <p className="bold" style={{ fontSize: '24px' }}>ESTE 03 DE MAYO DE 2026</p>
       <p className="bold" style={{ fontSize: '24px' }}>LLEVATE $15,000 PESOS</p>
       <p style={{ fontSize: '20px' }}>Costo por Boleto $100.00 MXN</p>
       <br />
